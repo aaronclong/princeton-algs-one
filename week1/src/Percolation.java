14e743a5b84c6b5abc3458815ffcd256964d42f7
Aaron Long <aaron.long.c@gmail.com>
2018-04-22 11:56:54 -0400

    Fixes some issues

--- week1/src/Percolation.java
+++ week1/src/Percolation.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 public class Percolation {
 
     private final int gridDimension;
@@ -15,28 +21,87 @@ public class Percolation {
 
     public void open(int row, int col) {
         if (isInRange(row, col)) {
-            grid[row][col] = 1;
+            grid[row - 1][col - 1] = 1;
             numberOfSites++;
         }
     }
 
     public boolean isOpen(int row, int col) {
         if (isInRange(row, col)) {
-            return grid[row][col] == 1;
+            return grid[row - 1][col - 1] == 1;
         }
         return false;
     }
 
-    public boolean isFull(int row, int col) { return true; }  // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (isInRange(row, col)) {
+            List<Cell> queue = new ArrayList<>();
+            //Set<Cell> visited = new HashSet<>();
+            queue.addAll(getSurroundingCells(row, col));
+
+            while(queue.size() != 0) {
+                Cell cell = queue.remove(0);
+                if (isRoof(cell)) {
+                    return true;
+                }
+                queue.addAll(getSurroundingCells(cell.getRow(), cell.getCol()));
+            }
+        }
+        return false;
+    }
+
+    private List<Cell> getSurroundingCells(int row, int col) {
+        List<Cell> surroundingCells = new ArrayList<>();
+        if (isOpen(row + 1, col)) {
+            Cell cell = new Cell(row + 1, col);
+            surroundingCells.add(cell);
+        }
+
+        if (isOpen(row - 1, col)) {
+            Cell cell = new Cell(row - 1, col);
+            surroundingCells.add(cell);
+        }
+
+        if (isOpen(row, col + 1)) {
+            Cell cell = new Cell(row, col + 1);
+            surroundingCells.add(cell);
+        }
+
+        if (isOpen(row, col - 1)) {
+            Cell cell = new Cell(row, col - 1);
+            surroundingCells.add(cell);
+        }
+
+        return surroundingCells;
+    }
+
+    private static boolean isRoof(Cell cell) {
+        return cell.getRow() == 1;
+    }
+
+    private static class Cell {
+        private final int row;
+        private final int col;
+
+        public Cell(int row, int col) {
+            this.row  = row;
+            this.col = col;
+        }
+
+        public int getRow() { return row; }
+        public int getCol() { return col; }
+    }
 
     public int numberOfOpenSites()  {
         return numberOfSites;
     }
 
-    public boolean percolates() { return true; }             // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
 
     private boolean isInRange(int row, int column) {
-        return gridDimension > row || column < gridDimension;
+        return ((row > 0 && column > 0 ) && (gridDimension >= row && column <= gridDimension));
     }
 
     public static void main(String[] args){}   // test client (optional)
--- week1/test/PercolationTest.java
+++ week1/test/PercolationTest.java
@@ -20,8 +20,48 @@ public class PercolationTest {
     public void assertNumberOfOpenCells() {
         Percolation percolation = new Percolation(5);
         percolation.open(1, 2);
-        percolation.open(0, 3);
+        percolation.open(3, 3);
         percolation.open(2, 1);
         Assert.assertEquals("Assert number of open cells", 3, percolation.numberOfOpenSites());
+    }
+
+    @Test
+    public void assertIsFullPositive() {
+        Percolation percolation = new Percolation(20);
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(3, 1);
+        percolation.open(4, 1);
+        percolation.open(5, 1);
+        Assert.assertEquals("Assert number of open cells", 5, percolation.numberOfOpenSites());
+        Assert.assertTrue("Assert cell is full", percolation.isFull(5, 1));
+        Assert.assertTrue("Assert cell is full", percolation.isFull(4, 1));
+        Assert.assertTrue("Assert cell is full", percolation.isFull(3, 1));
+        Assert.assertTrue("Assert cell is full", percolation.isFull(2, 1));
+        Assert.assertTrue("Assert cell is full", percolation.isFull(1, 1));
+    }
+
+    @Test
+    public void assertIsFullNegative() {
+        Percolation percolation = new Percolation(20);
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(3, 1);
+        percolation.open(4, 1);
+        percolation.open(5, 1);
+        Assert.assertEquals("Assert number of open cells", 5, percolation.numberOfOpenSites());
+        Assert.assertFalse("Assert cell is full", percolation.isFull(4, 17));
+    }
+
+    @Test
+    public void assertPercolates() {
+        Percolation percolation = new Percolation(5);
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(3, 1);
+        percolation.open(4, 1);
+        percolation.open(5, 1);
+        Assert.assertEquals("Assert number of open cells", 5, percolation.numberOfOpenSites());
+        Assert.assertTrue("Assert the percolator", percolation.percolates());
     }
 }
